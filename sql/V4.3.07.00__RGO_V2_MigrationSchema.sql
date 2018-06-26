f28a6356f7a2e55bb5ed93a9dde1d8c0ed03a677
szgooru <sachin@gooru.org>
2018-06-26 14:02:08 +0530

    adding schema for RGO v2 migration

---
+++ sql/V4.3.07.00__RGO_V2_MigrationSchema.sql
@@ -0,0 +1,106 @@
+CREATE TABLE learner_profile_competency_status_ts (
+  id bigserial PRIMARY KEY,
+  tx_subject_code text NOT NULL,
+  user_id text NOT NULL,
+  gut_code text NOT NULL,
+  status smallint NOT NULL DEFAULT 0 CHECK(status::smallint = ANY(ARRAY[0, 1, 2, 3, 4, 5])),
+  created_at timestamp NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+  updated_at timestamp NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+  UNIQUE(user_id, gut_code, status)
+);
+
+CREATE TABLE users_profile_master (
+  id bigserial PRIMARY KEY,
+  user_id text NOT NULL UNIQUE,
+  username text,
+  reference_id text,
+  email text,
+  login_type text,
+  first_name text,
+  last_name text,
+  parent_user_id text,
+  user_category text,
+  roles text,
+  birth_date date,
+  grade text,
+  course text,
+  thumbnail text,
+  gender text,
+  about text,
+  school_id text,
+  school text,
+  school_district_id text,
+  school_district text,
+  country_id text,
+  country text,
+  state_id text,
+  state text,
+  metadata text,
+  roster_id text,
+  roster_global_userid text,
+  tenant_root text,
+  tenant_id text,
+  partner_id text,
+  display_name text,
+  is_deleted boolean,
+  created_at timestamp without time zone NOT NULL,
+  updated_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL
+);
+
+CREATE TABLE user_grade_map (
+  id bigserial PRIMARY KEY,
+  user_id text NOT NULL,
+  subject_code text NOT NULL,
+  subject_name text NOT NULL,
+  grade text,
+  updated_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
+  UNIQUE (user_id, subject_code)
+);
+
+-- NOTE : the vector values are stored as integer and not real or decimal because of they being inexact
+-- We need to pick up a integer spread and then scale it down when it is returned by any API
+-- We shall take default spread as 1000 that is to say, table will store values from 0 to 1000
+CREATE TABLE user_vectors (
+  id bigserial PRIMARY KEY,
+  user_id text NOT NULL UNIQUE,
+  authority integer,
+  citizenship integer,
+  reputation integer,
+  updated_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL
+);
+
+
+--CREATE TABLE rep_monthly_user_competency_stats (
+--  id BIGSERIAL PRIMARY KEY,
+--  user_id text,
+--  in_progress int,
+--  completed int,
+--  mastered int,
+--  month int NOT NULL CHECK(month::int = ANY(ARRAY[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])),
+--  year int,
+--  updated_at timestamp NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+-- UNIQUE(user_id, month, year)
+--);
+
+--CREATE TABLE rep_monthly_user_competency (
+--  id BIGSERIAL PRIMARY KEY,
+--  user_id text,
+--  gut_code text,
+--  framework_code text,
+--  status smallint NOT NULL DEFAULT 0 CHECK(status::smallint = ANY(ARRAY[0, 1, 2, 3, 4, 5])),
+--  month int NOT NULL CHECK(month::int = ANY(ARRAY[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])),
+--  year int,
+--  updated_at timestamp NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+--  UNIQUE(user_id, gut_code, month, year)
+--);
+
+--CREATE TABLE rep_monthly_user_timespent_stats (
+--  id BIGSERIAL PRIMARY KEY,
+--  user_id text,
+--  content_type text,
+--  time_spent bigint,
+--  month int NOT NULL CHECK(month::int = ANY(ARRAY[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])),
+--  year int,
+--  updated_at timestamp NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+--  UNIQUE(user_id, content_type, month, year)
+--);
